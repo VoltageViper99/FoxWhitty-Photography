bb02d81eeb29a90ea0bc95bd277e0f401f5960a2
VoltageViper99 <voltagepcrepairs@protonmail.com>
2026-07-20 15:26:20 +1000

    refined menu.js to correct class naming

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("navmenu");
+const navMenu = document.getElementById("navbar");
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
